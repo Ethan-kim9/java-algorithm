b806ebd839bddb40232ea877521760c8451c5204
EthanKim <hey0507@gmail.com>
2021-09-01 23:09:12 +0900

    fail to solve supream

---
+++ algorithm/src/programmers/ch04/Main.java
@@ -0,0 +1,24 @@
+package programmers.ch04;
+
+
+// 다음에 다시 풉시다. 소수변환
+class Solution {
+    public int solution(int n) {
+        int count =0;
+        return count;
+    }
+}
+public class Main {
+    /*
+    * 1부터 입력받은 숫자 n 사이에 있는 소수의 개수를 반환하는 함수,
+    * solution을 만들어 보세요.
+    * 소수는 1과 자기 자신으로만 나누어지는 수를 의미합니다.
+    * (1은 소수가 아닙니다.)
+    **/
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        System.out.println(solution.solution(10));
+    }
+}
