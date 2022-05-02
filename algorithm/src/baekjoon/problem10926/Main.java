08fdcbc25848aac8f11367c81ed6b5d6e283d024
EthanKim <hey0507@gmail.com>
2022-05-02 15:04:18 +0900

    solved : baekjoonBasic - 기본 스캐너 및 버퍼리더를 이용한 문제 - Sysout 출력문제

---
+++ algorithm/src/baekjoon/problem10926/Main.java
@@ -0,0 +1,12 @@
+package baekjoon.problem10926;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(bufferedReader.readLine() + "??!");
+        bufferedReader.close();
+    }
+}
